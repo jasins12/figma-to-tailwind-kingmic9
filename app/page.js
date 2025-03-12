8cee9704a0f64ad4d022ae227dee7cb3cc3f5e83
jasins12 <michael.jasinski@mail.utoronto.ca>
2025-03-12 19:33:42 -0400

    Done 2

--- app/page.js
+++ app/page.js
@@ -3,12 +3,12 @@ export default function Home() {
   return (
     <div className="w-[1769px] h-[1151px] relative bg-gradient-to-b from-[#db4439] to-[#75241e] overflow-hidden">
       <div className="w-[150px] h-[52px] left-[1567px] top-[58px] absolute">
-        <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
-        <div className="left-[50px] top-0 absolute justify-start text-[#db4439] text-base font-black font-['Inknut_Antiqua']">Click</div>
+      <div className="w-[150px] h-[52px] left-0 top-0 absolute bg-[#eaecd7] rounded-[49px]" />
+        <div className="left-[50px] top-[-10px] absolute justify-start text-[#db4439] font-size-[25px] text-base font-black font-['Inknut_Antiqua']"><br></br>Click</div>
       </div>
       <div className="origin-top-left rotate-[-8.64deg] w-[543px] h-[590.40px] left-[1068.69px] top-[226px] absolute">
-        <div className="w-[543px] h-[590.40px] left-[88.69px] top-0 absolute bg-[#d9d9d9]" />
-        <img className="w-[630.99px] h-[1054.71px] left-[81.51px] top-[-57.62px] absolute" src="https://t4.ftcdn.net/jpg/01/16/61/93/240_F_116619399_YA611bKNOW35ffK0OiyuaOcjAgXgKBui.jpg" />
+
+        <img className="w-[630.99px] h-[900.71px] left-[81.51px] top-[-1.62px] absolute" src="https://t4.ftcdn.net/jpg/01/16/61/93/240_F_116619399_YA611bKNOW35ffK0OiyuaOcjAgXgKBui.jpg" />
       </div>
       <div className="w-[680px] left-[143px] top-[313px] absolute justify-start text-[#eaecd7] text-[110px] font-black font-['Inknut_Antiqua'] leading-[134.20px]">BlaBla <br></br> Coffe!</div>
       <div className="w-[680px] left-[148px] top-[740px] absolute justify-start text-[#eaecd7] text-[40px] font-semibold font-['Inknut_Antiqua'] leading-[48.80px]">Your NO.1 Choice</div>
